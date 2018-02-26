5da1dc00b3306448dcea6281a30db66f55538aba
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-25 22:00:43 -0600

    Reduce setup opts

--- setup.py
+++ setup.py
@@ -9,10 +9,4 @@ setup(
     name='wotw-cookiecutter-base',
     version=__version__,
     packages=[],
-    package_data={
-        '': [
-            'VERSION',
-        ]
-    },
-    include_package_data=True
 )
